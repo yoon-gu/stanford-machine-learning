054fa148218a947c38e3a538c942f191899906c4
Yoonku Hwang <yz0624@gmail.com>
2016-03-22 11:49:38 +0900

    assignment #4 완료!

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -81,7 +81,6 @@ for t = 1:m
     a_1 = A1(t, :)';
     z_2 = Z2(:, t);
     a_2 = A2(t, :)';
-    z_3 = Z2(:, t);
     a_3 = A3(t, :)';
     
     yy = Y(t, :)';
@@ -94,7 +93,8 @@ end
 Theta1_grad = Theta1_grad / m;
 Theta2_grad = Theta2_grad / m;
 % =========================================================================
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda / m * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda / m * Theta2(:, 2:end);
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
