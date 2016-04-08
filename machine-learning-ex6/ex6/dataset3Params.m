0c725011aea18df1ffde162e4ace8d452e23bd72
Yoonku Hwang <yz0624@gmail.com>
2016-04-08 20:48:22 +0900

    Parameters for dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+[Cs, sigmas] = meshgrid([0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]);
+Cs = Cs(:);sigmas = sigmas(:);
+errs = zeros(size(Cs));
+for j = 1:length(Cs)
+    C = Cs(j); sigma = sigmas(j);
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    errs(j) = mean(double(predictions ~= yval));
+end
 
+[a, b] = min(errs);
 
-
-
-
-
+C = Cs(b);
+sigma = sigmas(b);
 % =========================================================================
 
 end
