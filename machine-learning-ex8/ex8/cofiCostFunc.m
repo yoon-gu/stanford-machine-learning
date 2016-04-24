33e92c9104e55ece1d8493b759164a9b282b57ea
Yoonku Hwang <yz0624@gmail.com>
2016-04-25 01:18:45 +0900

    done assignment #8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,11 +43,11 @@ Theta_grad = zeros(size(Theta));
 
 
 d = (X * Theta' - Y).^2 .* R;
-J = sum(d(:)) / 2;
+J = sum(d(:)) / 2 + lambda / 2 * (sum(Theta(:).^2) + sum(X(:).^2));
 
 
-X_grad = (X * Theta' - Y) .* R * Theta;
-Theta_grad = (X * Theta' - Y)' .* R' * X;
+X_grad = (X * Theta' - Y) .* R * Theta + lambda * X;
+Theta_grad = (X * Theta' - Y)' .* R' * X + lambda * Theta;
 
 
 
